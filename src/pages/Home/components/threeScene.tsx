dc97d76d0db47a496d27b2a8648af32646dec173
pengfei.Ge <pengfei.ge@chintanneng.com>
2024-06-06 18:55:44 +0800

    feat: update

--- src/pages/Home/components/threeScene.tsx
+++ src/pages/Home/components/threeScene.tsx
@@ -78,19 +78,50 @@ const ThreeScene: React.FC = () => {
     scene.add(pointLight);
 
     // 创建并添加立方体
-    const boxGeometry = new THREE.BoxGeometry();
-    const boxMaterial = new THREE.MeshStandardMaterial({ color: 0x0000ff });
-    const cube = new THREE.Mesh(boxGeometry, boxMaterial);
-    cube.position.set(-2, 0.5, 0.5); // 将立方体移到左侧
+    // const boxGeometry = new THREE.BoxGeometry();
+    // const boxMaterial = new THREE.MeshStandardMaterial({ color: 0x0000ff });
+    // const cube = new THREE.Mesh(boxGeometry, boxMaterial);
+    // cube.position.set(-2, 0.5, 0.5); // 将立方体移到左侧
+    // scene.add(cube);
+    // 创建正方体的几何体和材质
+    const geometry = new THREE.BoxGeometry(1, 1, 1);
+    const material = new THREE.MeshStandardMaterial({
+      color: 0x0000ff,
+      transparent: true,
+      opacity: 0.5,
+    }); // 透视效果的蓝色材质
+    const cube = new THREE.Mesh(geometry, material);
+    cube.position.set(-2, 0.5, 0.5); // 将正方体移到左侧
     scene.add(cube);
 
+    // 创建正方体的边缘几何体
+    const edges = new THREE.EdgesGeometry(geometry, 0);
+    const lineMaterial = new THREE.LineBasicMaterial({ 
+        color: 0xffffff,
+        transparent: true,
+        opacity: 0.5,
+    }); // 白色线条材质
+    const wireframe = new THREE.LineSegments(edges, lineMaterial);
+    cube.add(wireframe); // 将线条添加到正方体中
     // 创建并添加圆柱体
     const cylinderGeometry = new THREE.CylinderGeometry(1, 1, 2, 32);
     const cylinderMaterial = new THREE.MeshStandardMaterial({
+      transparent: true,
       color: 0x0000ff,
+      opacity: 0.3,
     });
     const cylinder = new THREE.Mesh(cylinderGeometry, cylinderMaterial);
     cylinder.position.set(2, 1, 1); // 将圆柱体移到立方体右侧
+
+    const edges_cylinder = new THREE.EdgesGeometry(cylinderGeometry, 0);
+    const lineMaterial_cylinder = new THREE.LineBasicMaterial({ 
+        color: 0xffffff,
+        transparent: true,
+        opacity: 0.5,
+    }); // 白色线条材质
+    const wireframe_cylinder = new THREE.LineSegments(edges_cylinder, lineMaterial_cylinder);
+    cylinder.add(wireframe_cylinder); // 将线条添加到圆柱体中
+
     scene.add(cylinder);
 
     // 添加坐标轴
@@ -203,7 +234,7 @@ const ThreeScene: React.FC = () => {
 
       // 将鼠标位置归一化到设备坐标 (-1到1)
       const rect = mount.getBoundingClientRect();
-    //   console.log('rect', rect);
+      //   console.log('rect', rect);
       mouseRef.current.x =
         ((event.clientX - rect.left) / mount.clientWidth) * 2 - 1;
       mouseRef.current.y =
@@ -212,7 +243,7 @@ const ThreeScene: React.FC = () => {
       // 更新射线投射器
       raycasterRef.current.setFromCamera(mouseRef.current, camera);
       // 可视化射线
-      visualizeRaycaster(raycasterRef.current, scene);
+    //   visualizeRaycaster(raycasterRef.current, scene);
       // 检测交互对象
       const intersects = raycasterRef.current.intersectObjects(
         meshObjects,
@@ -234,6 +265,8 @@ const ThreeScene: React.FC = () => {
           selectedObject.userData.originalMaterial = selectedObject.material;
           selectedObject.material = new THREE.MeshBasicMaterial({
             color: 0xff0000,
+            transparent: true,
+            opacity: 0.5,
           });
         } else {
           // 未点击到任何对象，重置高亮对象为null
